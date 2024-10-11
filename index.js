d28e37d0b6326b03bda73b5b32545016809b1b7e
assa mohammed <mohammedassad749@gmail.com>
2024-10-11 15:49:48 +0300

    add changes

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(a, b) {
+    return a / b;
+    console.log(a / b);
+}
+divide(2000, 100);
+const square = x => {return x * x};
+square(2)
+console.log(square(2));
+const add = (a, b) => {return a + b}
+add(3,4)
+console.log(add(3,4));
